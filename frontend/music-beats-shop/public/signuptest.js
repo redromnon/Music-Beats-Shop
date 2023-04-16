215990dbb2458a40676d0084a64a41b12687487c
GaurangMusale <64917293+GaurangMusale@users.noreply.github.com>
2023-04-16 13:00:34 +0530

    Update signuptest.js

--- frontend/music-beats-shop/public/signuptest.js
+++ frontend/music-beats-shop/public/signuptest.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Container, Row, Col, Panel, FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
 
 function SignUp() {
   const [username, setUsername] = useState('');
@@ -23,65 +24,55 @@ function SignUp() {
   }
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6 col-sm-offset-3">
-          <div className="panel panel-default">
-            <div className="panel-heading">
+    <Container>
+      <Row>
+        <Col md={6} mdOffset={3}>
+          <Panel>
+            <Panel.Heading>
               <h2 className="text-center">Create Account</h2>
-            </div>
-            <div className="panel-body">
+            </Panel.Heading>
+            <Panel.Body>
               <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label htmlFor="username">Username:</label>
-                  <input
+                <FormGroup>
+                  <ControlLabel>Username:</ControlLabel>
+                  <FormControl
                     type="text"
-                    className="form-control"
-                    id="username"
                     placeholder="Enter username"
                     value={username}
                     onChange={handleUsernameChange}
                   />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email">Email:</label>
-                  <input
+                </FormGroup>
+                <FormGroup>
+                  <ControlLabel>Email:</ControlLabel>
+                  <FormControl
                     type="email"
-                    className="form-control"
-                    id="email"
                     placeholder="Enter email"
                     value={email}
                     onChange={handleEmailChange}
                   />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password:</label>
-                  <input
+                </FormGroup>
+                <FormGroup>
+                  <ControlLabel>Password:</ControlLabel>
+                  <FormControl
                     type="password"
-                    className="form-control"
-                    id="password"
                     placeholder="Enter password"
                     value={password}
                     onChange={handlePasswordChange}
                     minLength={8}
                     pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)"
                   />
-                  <small className="form-text text-muted">
+                  <HelpBlock>
                     Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.
-                  </small>
-                </div>
-                <div className="form-group text-center">
-                  <button type="submit" className="btn btn-primary">Signup</button>
-                </div>
-                <div className="form-group text-center">
-                  <a href="login.html">Already have an Account?</a>
-                </div>
+                  </HelpBlock>
+                </FormGroup>
+                <Button type="submit" bsStyle="primary" block>Signup</Button>
+                <p className="text-center">Already have an account? <a href="login.html">Login</a></p>
               </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </Panel.Body>
+          </Panel>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
