2925039c859d9402b0f05051d40fc79be004dbc2
redromnon <rishimoharir@gmail.com>
2023-04-17 18:55:17 +0530

    Add post api example

--- backend/base.py
+++ backend/base.py
@@ -1,12 +1,36 @@
-from flask import Flask
+#This script is purely for testing
+
+from flask import Flask, request
 
 api = Flask(__name__)
 
-@api.route('/profile')
+@api.route('/profile', methods=["GET", "POST"])
 def my_profile():
-    response_body = {
-        "name": "Rishabh",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
+        
+    if request.method == "GET":
+        response_body = {
+            "name": "Rishabh",
+            "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        }
 
-    return response_body
+        return response_body
+    else:
+        response_body = {
+            "name": f"{request.args.get('name')}",
+            "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        }
+
+'''
+How to define POST method:
+- Add methods arg to route() where method=["GET", "POST"]
+- Use request class to get the passed variable (value) 
+  from frontend and store in a (key)
+  Eg) Below 'text' is the key and whatever value is sent 
+  from frontend is the value
+'''
+@api.route('/tagline', methods=["GET", "POST"])
+def tagline():
+    
+    word = request.args.get('text')
+    
+    return f'You wanted {word}, right?'
